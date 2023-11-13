32514b833dbcaab5ed676349250c86317905ec42
Omar-will <omar.bengoudifa@outlook.fr>
2023-11-13 12:20:24 +0100

    modification token

--- src/redux/reducers/token.js
+++ src/redux/reducers/token.js
@@ -1,14 +1,16 @@
 export function splitAndStoreToken(token) {
-   
+
     const tokenPart2 = token.slice(token.length / 2);
 
-    localStorage.setItem("secure_token_part2", tokenPart2);
+    localStorage.setItem("secure_token_part2", encodeURIComponent(tokenPart2));
 }
 
 export function combineStoredToken() {
     const cookies = document.cookie.split(";");
-    const tokenPart2 = localStorage.getItem("secure_token_part2");
+
     let tokenPart1 = null;
+    let tokenPart2 = localStorage.getItem("secure_token_part2");
+
     for (const cookie of cookies) {
         const [name, value] = cookie.trim().split("=");
         if (name === "secure_token_part1") {
@@ -16,15 +18,17 @@ export function combineStoredToken() {
             break;
         }
     }
+
     if (!tokenPart1 || !tokenPart2) {
         return null;
     }
-    const completeToken = tokenPart1 + tokenPart2;
+
+    const completeToken = tokenPart1 + decodeURIComponent(tokenPart2);
 
     return completeToken;
 }
 
 export function clearStoredToken() {
-
+    
     localStorage.removeItem("secure_token_part2");
 }
